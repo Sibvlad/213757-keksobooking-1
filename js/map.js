5d6cceedf2fbd5574b424c587b1f5c46c7cee71f
Владислав Сибирев <sibvlad88@mail.ru>
2017-09-12 20:38:59 +0300

    Максимум подвижности

--- js/map.js
+++ js/map.js
@@ -18,6 +18,8 @@ window.map = (function () {
       tokyoPinMap.addEventListener('keydown', keydownHandler);
       dialogClose.addEventListener('click', window.card.closePanel);
       dialogClose.addEventListener('keydown', closeKeydownHandle);
+      var mainPin = document.querySelector('.pin__main');
+      mainPin.addEventListener('mousedown', onPinMove);
     },
     getApp: function (id) {
       for (var j = 0; j < apartments.length; j++) {
@@ -29,6 +31,47 @@ window.map = (function () {
     },
     dialog: document.querySelector('.dialog')
   };
+
+
+  function onPinMove(evt) {
+    evt.preventDefault();
+
+    var target = evt.target.closest('.pin');
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      evt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+      target.style.top = (target.offsetTop - shift.y) + 'px';
+      target.style.left = (target.offsetLeft - shift.x) + 'px';
+
+      var address = document.querySelector('#address');
+      address.value = 'x: ' + (target.offsetLeft + 32) + ', y: ' + (target.offsetTop + 94);
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      tokyoPinMap.removeEventListener('mousemove', onMouseMove);
+      tokyoPinMap.removeEventListener('mouseup', onMouseUp);
+    };
+
+    tokyoPinMap.addEventListener('mousemove', onMouseMove);
+    tokyoPinMap.addEventListener('mouseup', onMouseUp);
+  }
 
 
   function keydownHandler(event) {
